f556f95bd4028ab2e15835e94df31f0e116a903e
Zixuan Zhang <zixuanzhang.x@gmail.com>
2021-02-12 13:02:30 -0600

    Add api test route and register api blueprint

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
+from api.api import api
+
+from db import db
+
 app = Flask(__name__)
+
+# Database setup
+@app.before_first_request
+def initialize():
+    db.setup()
+
+# Register Blueprints
+app.register_blueprint(api, url_prefix='/api')
