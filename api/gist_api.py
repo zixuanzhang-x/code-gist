b5a50ca123108ea2c280740af43aac152521a0e3
Zixuan Zhang <zixuanzhang.x@gmail.com>
2021-02-28 21:18:36 -0600

    Update user and gist apis

--- api/gist_api.py
+++ api/gist_api.py
@@ -7,6 +7,10 @@ gist_api_blueprint = Blueprint('gist_api_blueprint', __name__)
 
 @gist_api_blueprint.route('/gist', methods=['GET'])
 def get_gists():
+    """
+    Returns a list of gists whose gist_name matchs 'q'.
+    All gists are matched if q is not provided.
+    """
     q = request.args.get('q', None)
     with db.get_db_cursor(True) as cursor:
         if q:  # search for gists match pattern q
@@ -19,13 +23,12 @@ def get_gists():
 
 @gist_api_blueprint.route('/gist', methods=['POST'])
 def create_gists():
+    """Create a new gist."""
     user_id = request.form.get('user_id')
     gist_name = request.form.get('gist_name')
     user_name = request.form.get('user_name')
     description = request.form.get('description')
     content = request.form.get('content')
-    is_forked = request.form.get('is_forked', False)
-    forked_from = request.form.get('forked_from', None)
     with db.get_db_cursor(True) as cursor:
         cursor.execute(
             """INSERT INTO gist(user_id,
@@ -34,12 +37,10 @@ def create_gists():
                                 description,
                                 content, 
                                 created,
-                                last_modified,
-                                is_forked,
-                                forked_from)
-               VALUES (%s, %s, %s, %s, %s, current_timestamp, current_timestamp, %s, %s)
+                                last_modified)
+               VALUES (%s, %s, %s, %s, %s, current_timestamp, current_timestamp)
                RETURNING user_id;""",
-            (user_id, gist_name, user_name, description, content, is_forked, forked_from))
+            (user_id, gist_name, user_name, description, content))
         gist_id = cursor.fetchone()[0]
         current_app.logger.info(
             f"Created new gist: {{'gist_id': {gist_id}, 'user_id': {user_id}}}")
@@ -56,11 +57,33 @@ def get_gist(gist_id):
 
 @gist_api_blueprint.route('/gist/<int:gist_id>', methods=['PUT'])
 def update_gist(gist_id):
-    return {}
+    with db.get_db_cursor(True) as cursor:
+        cursor.execute("SELECT * FROM gist WHERE gist_id = %s;;", (gist_id,))
+        if cursor.rowcount == 0:
+            return jsonify(error=400, text='gist_id not found'), 400
+        else:
+            gist = extract_gists_from_cursor(cursor)[0]
+            gist_name = request.form.get('gist_name')
+            description = request.form.get('description')
+            content = request.form.get('content')
+
+            cursor.execute("""UPDATE gist
+                              SET gist_name = %s,
+                                  description = %s,
+                                  content = %s,
+                                  last_modified = current_timestamp
+                              WHERE gist_id = %s;""", (gist_name or gist['gist_name'],
+                                                       description or gist['description'],
+                                                       content or gist['content'],
+                                                       gist_id))
+
+            cursor.execute("SELECT * FROM gist WHERE gist_id = %s;;", (gist_id,))
+            return jsonify(extract_gists_from_cursor(cursor))
 
 
 @gist_api_blueprint.route('/gist/<int:gist_id>/comment', methods=['GET'])
 def get_gist_comments(gist_id):
+    """Return a list of comments of this gist."""
     with db.get_db_cursor(True) as cursor:
         cursor.execute(
             "SELECT * FROM gist_comment WHERE gist_id = %s;;", (gist_id,))
@@ -69,6 +92,7 @@ def get_gist_comments(gist_id):
 
 @gist_api_blueprint.route('/gist/<int:gist_id>/comment', methods=['POST'])
 def create_gist_comments(gist_id):
+    """Create a new comment under this gist."""
     content = request.form.get('content')
     user_id = request.form.get('user_id')
 
@@ -79,27 +103,61 @@ def create_gist_comments(gist_id):
                 VALUES (%s, current_timestamp, %s, %s)
                 RETURNING comment_id;""",
                 (content, user_id, gist_id))
-        
+
             comment_id = cursor.fetchone()[0]
             current_app.logger.info(
                 f"Created new comment: {{'comment_id': {comment_id}, 'gist_id': {gist_id}, 'user_id': {user_id}}}")
-            
-            # todo: update gist on number of comments
+
+            cursor.execute("UPDATE gist SET comments = comments + 1 WHERE gist_id = %s", (gist_id,))
 
             return {'comment_id': comment_id}
         except Exception as e:
-            return jsonify(error=404, text=str(e)), 404
-
-
-@gist_api_blueprint.route('/gist/<int:gist_id>/fork', methods=['GET'])
-def get_gist_forks(gist_id):
-    return {}
+            return jsonify(error=400, text=str(e)), 400
 
 
 @gist_api_blueprint.route('/gist/<int:gist_id>/star', methods=['GET'])
 def get_gist_stars(gist_id):
+    """Return a list of users that starred this gist"""
     with db.get_db_cursor(True) as cursor:
         cursor.execute("""SELECT gist_user.* FROM star
                           JOIN gist_user ON star.user_id = gist_user.user_id
                           WHERE star.gist_id = %s;""", (gist_id,))
         return jsonify(extract_users_from_cursor(cursor))
+
+
+@gist_api_blueprint.route('/gist/<int:gist_id>/star', methods=['POST'])
+def create_gist_star(gist_id):
+    """Create a star relationship between a user and this gist"""
+    user_id = request.form.get('user_id')
+    with db.get_db_cursor(True) as cursor:
+        try:
+            cursor.execute(
+                "INSERT INTO star(user_id, gist_id) VALUES (%s, %s);", (user_id, gist_id))
+
+            current_app.logger.info(
+                f"""Created new star: {{'user_id': {user_id}, 'gist_id': {gist_id}}}""")
+
+            cursor.execute("UPDATE gist SET stars = stars + 1 WHERE gist_id = %s", (gist_id,))
+
+            return {'user_id': user_id, 'gist_id': gist_id}
+        except Exception as e:
+            return jsonify(error=400, text=str(e)), 400
+        
+
+@gist_api_blueprint.route('/gist/<int:gist_id>/star', methods=['DELETE'])
+def delete_gist_star(gist_id):
+    """Delete a star relationship between a user and this gist"""
+    user_id = request.form.get('user_id')
+    with db.get_db_cursor(True) as cursor:
+        try:
+            cursor.execute(
+                "DELETE FROM star WHERE user_id = %s AND gist_id = %s;", (user_id, gist_id))
+
+            current_app.logger.info(
+                f"""Deleted an old star: {{'user_id': {user_id}, 'gist_id': {gist_id}}}""")
+
+            cursor.execute("UPDATE gist SET stars = stars - 1 WHERE gist_id = %s", (gist_id,))
+
+            return {'user_id': user_id, 'gist_id': gist_id}
+        except Exception as e:
+            return jsonify(error=400, text=str(e)), 400
--- api/user_api.py
+++ api/user_api.py
@@ -13,7 +13,6 @@ def get_or_create_user():
     Return user's PostgreSQL id.
     """
     auth0_id = request.form.get('auth0_id')
-    picture = request.form.get('picture')
     with db.get_db_cursor(True) as cursor:
         cursor.execute(
             "SELECT user_id from gist_user WHERE auth0_id = %s;", (auth0_id,))
@@ -21,29 +20,29 @@ def get_or_create_user():
         if cursor.rowcount == 1:  # user already exists
             user_id = cursor.fetchone()[0]
         else:  # user does not exists yet, create new user
+            picture = request.form.get('picture')
+            user_name = request.form.get('user_name')
             cursor.execute(
-                "INSERT INTO gist_user(auth0_id, picture) VALUES(%s, %s) RETURNING user_id;", (auth0_id, picture))
+                "INSERT INTO gist_user(auth0_id, user_name, picture) VALUES(%s, %s, %s) RETURNING user_id;", (auth0_id, user_name, picture))
             user_id = cursor.fetchone()[0]
             current_app.logger.info(
-                f"Created new user: {{'user_id': {user_id}, 'auth0_id': {auth0_id}}}")
-                
+                f"Created new user: {{'user_id': {user_id}, 'user_name': {user_name}, 'auth0_id': {auth0_id}}}")
+
         return {'user_id': user_id}
 
 
 @user_api_blueprint.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
+    """Return this user with the user_id."""
     with db.get_db_cursor(True) as cursor:
         cursor.execute(
             "SELECT * FROM gist_user WHERE user_id = %s", (user_id,))
         return jsonify(extract_users_from_cursor(cursor))
 
-@user_api_blueprint.route('/user/<int:user_id>/fork', methods=['GET'])
-def get_user_forks(user_id):
-    return {}
-
 
 @user_api_blueprint.route('/user/<int:user_id>/star', methods=['GET'])
 def get_user_stars(user_id):
+    """Return a list of gists that this user starred."""
     with db.get_db_cursor(True) as cursor:
         cursor.execute("""SELECT gist.* FROM star
                           JOIN gist ON star.gist_id = gist.gist_id
@@ -53,6 +52,7 @@ def get_user_stars(user_id):
 
 @user_api_blueprint.route('/user/<int:user_id>/gist', methods=['GET'])
 def get_user_gists(user_id):
-     with db.get_db_cursor(True) as cursor:
+    """Return a list of gists that belong to this user."""
+    with db.get_db_cursor(True) as cursor:
         cursor.execute("SELECT * FROM gist WHERE user_id = %s;;", (user_id,))
         return jsonify(extract_gists_from_cursor(cursor))
