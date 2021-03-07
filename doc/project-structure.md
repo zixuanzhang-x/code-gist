79ee022ddd878a4705559877faad415a4c5f6570
Zhaoyu Wang <wzy0794@gmail.com>
2021-03-07 22:20:26 +0800

    Refactored login_util&discover.js

--- doc/project-structure.md
+++ doc/project-structure.md
@@ -3,44 +3,71 @@ gist/
 |
 ├── api/
 |   ├── __init__.py
-|   └── api.py
+|   └── gist_api.py
+|   └── user_api.py
 |
 ├── auth/
-|   ├── templates/
-|   |   └── auth/
-|   |       ├── login.html
-|   |       ├── forgot_password.html
-|   |       └── signup.html
-|   |
 |   ├── __init__.py
 |   └── auth.py
 |
+├── db/
+|   ├── db.py
+|   └── schemas.sql
+|
 ├── general/
-|   ├── templates/
-|   |   └── general/
-|   |       └── index.html
-|   |
 |   ├── __init__.py
 |   └── general.py
 |
-├── gists/
-|   ├── static/
-|   |
-|   ├── templates/
-|   |   └─ gists/
-|   |       └── view.html
-|   |
-|   ├── __init__.py
-|   └── products.py
-|
 ├── static/
-|   ├── logo.png
-|   ├── main.css
-|   └── generic.js
+|   ├── css/
+|   |   ├── base.css
+|   |   ├── create.css
+|   |   ├── discover.css
+|   |   └── main.css
+|   |
+|   ├── img/
+|   |   ├── comment.png
+|   |   ├── logo.png
+|   |   ├── star.png
+|   |
+|   └── js/
+|       ├── base.js
+|       ├── create.js
+|       ├── discover.js
+|       ├── gist.js
+|       └── user.js
 |
-|── templates/
+├── templates/
+|   ├── base.html
+|   ├── footer.html
+|   |
+|   ├── error_page/
+|   |   ├── 404.html
+|   |   ├── 410.html
+|   |   └── 500.html
+|   |
+|   └── gist_page/
+|       ├── creatre.html
+|       ├── discover.html
+|       ├── gist_stargazers.html
+|       ├── gist.html
+|       └── user.html
+|
+├── utils/
+|   ├── api_util.py
+|   ├── error_page_util.py
+|   ├── login_util.py
+|   └── time_util.py
+|
+├── .gitignore
 |
 ├── app.py
-└── models.py
-# ├── config.py
+|
+├── Pipfile
+|
+├── Pipfile.lock
+|
+├── Procfile
+|
+└── README.md
 ```
--- utils/login_util.py
+++ utils/login_util.py
@@ -1,10 +1,55 @@
-from flask import redirect, session, url_for
+import os
+import requests
+
+from flask import Blueprint, redirect, session, url_for
 from functools import wraps
+from urllib.parse import urlencode
+
+from auth.auth0 import auth0
+
+auth_blueprint = Blueprint('auth_blueprint', __name__)
+
+API_URI = os.environ["API_URI"]
+
+@auth_blueprint.route('/login')
+def user_login():
+    if 'profile' in session:
+        return redirect(url_for('route_blueprint.discover_page'))
+    else:
+        return auth0().authorize_redirect(redirect_uri=url_for('auth_blueprint.callback', _external=True))
+
+@auth_blueprint.route('/callback')
+def callback():
+    # register by auth0
+    auth0().authorize_access_token()
+    response = auth0().get('userinfo')
+    userinfo = response.json()
+
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+
+    requests.post(API_URI + '/api/user', \
+        data={'auth0_id': userinfo['sub'],
+              'user_name': userinfo['name'],
+              'picture': userinfo['picture'],
+              })
+
+    return redirect(url_for('route_blueprint.create_page'))
+
+@auth_blueprint.route('/logout')
+def user_logout():
+    session.clear()
+    params = { 'returnTo': url_for('route_blueprint.discover_page', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID'] }
+    return redirect(auth0().api_base_url + '/v2/logout?' + urlencode(params))
 
 def login_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if 'profile' not in session:
-            return redirect(url_for('route_blueprint.user_login'))
+            return redirect(url_for('auth_blueprint.user_login'))
         return f(*args, **kwargs)
     return decorated
