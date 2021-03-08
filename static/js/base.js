97e9a05c6a4fa3482e1f1494e1013b7e925ec505
Ziiiiing <cheng904@umn.edu>
2021-03-09 04:35:20 +0800

    added logged in user's picture

--- static/js/base.js
+++ static/js/base.js
@@ -16,6 +16,20 @@ $(document).ready(function(){
         }
     })
 
+    // get current user picture
+    var user_pic = ''
+    $.ajax({
+        url: '/api/user/' + user_id,
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            user_pic = data[0].picture
+        }
+    })
+
+    document.getElementById('current-user-pic').src = user_pic
+
     // set href for redirecting to current user page
     document.getElementById('current_user').setAttribute('href', `/user/${user_id}`)
     document.getElementById('current_gists').setAttribute('href', `/user/${user_id}`)
